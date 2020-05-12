befb0eddfe89bae0d8a63ec7b3f52b1739ee3221
Yurii <yurii.atamanchuk@zebra.com>
2020-05-12 23:30:27 +0300

    nicer doc

--- src/main/scala/org/scanet/optimizers/SGD.scala
+++ src/main/scala/org/scanet/optimizers/SGD.scala
@@ -7,14 +7,13 @@ import org.scanet.math.syntax._
 case class SGD(rate: Double = 0.01, momentum: Double = 0.0, nesterov: Boolean = false) extends Algorithm[Tensor[Float]] {
 
   override def delta[A: Numeric : TensorType, B: Numeric : TensorType](f: TensorFunction[A, B], initArg: Output[A], prevState: Tensor[Float]): (Output[Float], Tensor[Float]) = {
-    // op = +|-
     // Nesterov == true:
-    //   v[i] = momentum * v[i-1] + rate * grad(arg op momentum * v[i-1])
+    //   v[i] = momentum * v[i-1] + rate * grad(arg - momentum * v[i-1])
     // Nesterov == false:
     //   v[i] = momentum * v[i-1] + rate * grad(arg)
     // momentum == 0.0 (vanilla SGD):
     //   v[i] = rate * grad(arg)
-    // arg = arg op v[i]
+    // arg = arg - v[i]
 
     val m = momentum.toFloat.const
     // TODO: should this be + for maximize?
