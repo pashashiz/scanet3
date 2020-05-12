9c206b76a608c767f3cd7056b21a068fef6c51c2
Yurii <yurii.atamanchuk@zebra.com>
2020-05-12 23:25:35 +0300

    trait

--- src/main/scala/org/scanet/optimizers/Algorithm.scala
+++ src/main/scala/org/scanet/optimizers/Algorithm.scala
@@ -3,7 +3,7 @@ package org.scanet.optimizers
 import org.scanet.core.{Output, Tensor, TensorType}
 import org.scanet.math.Numeric
 
-abstract class Algorithm[S] {
+trait Algorithm[S] {
 
   def delta[A: Numeric : TensorType, B: Numeric : TensorType](f: TensorFunction[A, B], initArg: Output[A], prevState: S): (Output[Float], S)
 
