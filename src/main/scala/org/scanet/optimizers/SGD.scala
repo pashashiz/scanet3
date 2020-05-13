7346f94dd7d50752a7d55866a8d9af700d6e683e
Yura Atamanchuk <yura.ata@gmail.com>
2020-05-13 13:33:15 +0300

    SGD (#28)
    * SGD with Nesterov acceleration
    
    * trait
    
    * nicer doc
    
    * nesterov acceleration by calculating velocity twice (instead of calculating grad from prev velocity)
    
    * correct state and better naming
    
    Co-authored-by: Yurii <yurii.atamanchuk@zebra.com>

---
+++ src/main/scala/org/scanet/optimizers/SGD.scala
@@ -0,0 +1,19 @@
+package org.scanet.optimizers
+
+import org.scanet.core.{Output, Tensor, TensorType}
+import org.scanet.math.Numeric
+import org.scanet.math.syntax._
+
+case class SGD(rate: Double = 0.01, momentum: Double = 0.0, nesterov: Boolean = false) extends Algorithm[Tensor[Float]] {
+
+  override def delta[A: Numeric : TensorType, B: Numeric : TensorType](f: TensorFunction[A, B], initArg: Output[A], prevState: Tensor[Float]): (Output[Float], Tensor[Float]) = {
+    val g = f.grad(initArg)
+    val m = momentum.toFloat.const
+    val r = rate.toFloat.const
+    val v = r * g - m * prevState.const
+    val delta = if (nesterov) r * g - m * v else v
+    (delta, v.eval)
+  }
+
+  override def initState[A: Numeric : TensorType](initArg: Tensor[A]): Tensor[Float] = Tensor.zeros[Float](initArg.shape)
+}
