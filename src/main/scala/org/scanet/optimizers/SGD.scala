f70d46588ad4efce5992d3912a97ad223f9e4a87
Yurii <yurii.atamanchuk@zebra.com>
2020-05-13 13:09:05 +0300

    correct state and better naming

--- src/main/scala/org/scanet/optimizers/SGD.scala
+++ src/main/scala/org/scanet/optimizers/SGD.scala
@@ -11,9 +11,8 @@ case class SGD(rate: Double = 0.01, momentum: Double = 0.0, nesterov: Boolean = 
     val m = momentum.toFloat.const
     val r = rate.toFloat.const
     val v = r * g - m * prevState.const
-    val velocity = if (nesterov) r * g - m * v else v
-    val res = velocity.eval
-    (res.const, res)
+    val delta = if (nesterov) r * g - m * v else v
+    (delta, v.eval)
   }
 
   override def initState[A: Numeric : TensorType](initArg: Tensor[A]): Tensor[Float] = Tensor.zeros[Float](initArg.shape)
