fffc7a49ef29d9be9cb2f62e0e8e4e5f7b5b20f6
Pavlo Pohrebnyi <pogrebnij@gmail.com>
2020-05-14 13:59:24 +0300

    Added pow op (#30)
    Added `pow` op

--- src/test/scala/org/scanet/math/MathBaseOpSpec.scala
+++ src/test/scala/org/scanet/math/MathBaseOpSpec.scala
@@ -420,6 +420,23 @@ class MathBaseOpSpec extends AnyFlatSpec with Matchers {
     ((a :* x).sum grad x).eval should be(grad)
   }
 
+  "pow" should "compute the power of the tensor with high exponent" in {
+    Tensor.vector(1.0f, 2.0f, 3.0f).const.pow(2).eval should be(Tensor.vector(1.0f, 4.0f, 9.0f))
+  }
+
+  it should "return same tensor if exponent is 1" in {
+    Tensor.vector(1.0f, 2.0f, 3.0f).const.pow(1).eval should be(Tensor.vector(1.0f, 2.0f, 3.0f))
+  }
+
+  it should "return ones tensor if exponent is 0" in {
+    Tensor.vector(1.0f, 2.0f, 3.0f).const.pow(0).eval should be(Tensor.vector(1.0f, 1.0f, 1.0f))
+  }
+
+  it should "calculate a gradient" in {
+    val x = Tensor.vector(5, 10, 15).const
+    x.pow(3).sum.grad(x).eval should be(Tensor.vector(75.0f, 300.0f, 675.0f))
+  }
+
   "sum" should "calculate sum across all axises by default" in {
     Tensor.matrix(Array(1, 2, 3), Array(4, 5, 6)).const.sum.eval should be(Tensor.scalar(21))
   }
