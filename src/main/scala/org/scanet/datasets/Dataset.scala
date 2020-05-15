5e6547fc6e1a4cbd280e7c04aa4aaf277aa7ce02
Yurii <yurii.atamanchuk@zebra.com>
2020-05-15 20:22:51 +0300

    ensure constant batch size - if last batch is smaller than requested size - fill it with zeros

--- src/main/scala/org/scanet/datasets/Dataset.scala
+++ src/main/scala/org/scanet/datasets/Dataset.scala
@@ -5,39 +5,45 @@ import org.scanet.math.Numeric
 import org.scanet.math.syntax._
 
 import scala.io.Source
+import scala.reflect.ClassTag
 
 trait Dataset[X] {
   def iterator: Iterator[X]
   def shape(batch: Int): Shape
+  def size: Int
 }
 
 trait Iterator[X] {
   def hasNext: Boolean
   def next(batch: Int): Tensor[X]
-  def size: Option[Int]
 }
 
-case class TensorDataset[X: TensorType: Numeric](src: Tensor[X]) extends Dataset[X] {
+case class TensorDataset[X: TensorType: Numeric: ClassTag](src: Tensor[X]) extends Dataset[X] {
   override def iterator: Iterator[X] = new Iterator[X] {
     var pos: Int = 0
-    val size: Option[Int] = Some(src.shape.dims.head)
     override def hasNext: Boolean = src.shape.dims.head > pos
     override def next(batch: Int): Tensor[X] = {
       require(hasNext, "dataset has no elements left")
-      // TODO: ensure constant shape
-      val slice: Tensor[X] = src(pos until math.min(pos + batch, size.get))
+      val slice =
+        if (batch > size) src(pos until size)
+        else if (pos + batch <= size) src(pos until pos + batch)
+        else {
+          val zero = Numeric[X].zero
+          val reminder = (pos + batch - size) * src.shape.tail.power
+          val array = src(pos until size).toArray appendedAll Array.fill(reminder)(zero)
+          Tensor(array, shape(batch))
+        }
       pos = pos + slice.shape.dims.head
       slice
     }
   }
-
+  val size: Int = src.shape.dims.head
   override def shape(batch: Int): Shape = src.view.narrow(Projection(0 until batch)).shape
 }
 
 case class EmptyDataset[X: TensorType: Numeric]() extends Dataset[X] {
   override def iterator: Iterator[X] = new Iterator[X] {
     var completed = false
-    override def size: Option[Int] = Some(0)
     override def hasNext: Boolean = !completed
     override def next(batch: Int): Tensor[X] = {
       completed = true
@@ -46,6 +52,7 @@ case class EmptyDataset[X: TensorType: Numeric]() extends Dataset[X] {
   }
 
   override def shape(batch: Int): Shape = Shape()
+  val size: Int = 0
 }
 
 case class CSVDataset(path: String) extends Dataset[Float] {
@@ -56,24 +63,28 @@ case class CSVDataset(path: String) extends Dataset[Float] {
       .map(_.split(",").map(_.toFloat))
       .toVector
   }
-
   private lazy val columns: Int = data(0).length
+
+  lazy val size: Int = data.size
 
   override def iterator: Iterator[Float] = new Iterator[Float] {
     private var pos: Int = 0
 
-    val size: Option[Int] = Some(data.size)
-
-    override def hasNext: Boolean = size.get > pos
+    override def hasNext: Boolean = size > pos
     override def next(batch: Int): Tensor[Float] = {
       require(hasNext, "dataset has no elements left")
-      // TODO: ensure constant shape
-      val slice = data.slice(pos, math.min(pos + batch, size.get))
-      pos = pos + slice.size
+      val slice =
+        if (batch > size) data
+        else if (pos + batch <= size) data.slice(pos, pos + batch)
+        else {
+          val reminder = Vector.fill(pos + batch - size)(Array.fill(columns)(0f))
+          data.slice(pos, size) appendedAll reminder
+        }
+      pos = pos + batch
       val x = slice.flatten.toArray
       Tensor(x, Shape(slice.size, columns))
     }
   }
 
-  override def shape(batch: Int): Shape = Shape(batch, columns)
+  override def shape(batch: Int): Shape = Shape(if (batch < size) batch else size, columns)
 }
--- src/test/scala/org/scanet/datasets/DatasetSpec.scala
+++ src/test/scala/org/scanet/datasets/DatasetSpec.scala
@@ -1,9 +1,8 @@
-package org.scanet.optimizers
+package org.scanet.datasets
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scanet.core.Tensor
-import org.scanet.datasets.TensorDataset
 import org.scanet.math.syntax._
 
 class DatasetSpec extends AnyFlatSpec with Matchers {
@@ -13,10 +12,10 @@ class DatasetSpec extends AnyFlatSpec with Matchers {
     it.hasNext should be(true)
     it.next(2) should be(Tensor.vector(1, 2))
     it.hasNext should be(true)
-    it.next(2) should be(Tensor.vector(3))
+    it.next(2) should be(Tensor.vector(3, 0))
   }
 
-  "tensor dataset" should "fail when requested batch is out of bound" in {
+  it should "fail when requested batch is out of bound" in {
     val it = TensorDataset(Tensor.vector(1, 2, 3)).iterator
     it.hasNext should be(true)
     it.next(3) should be(Tensor.vector(1, 2, 3))
@@ -24,5 +23,29 @@ class DatasetSpec extends AnyFlatSpec with Matchers {
     the [IllegalArgumentException] thrownBy {
       it.next(3)
     } should have message "requirement failed: dataset has no elements left"
+  }
+
+  "CSV dataset" should "read records" in {
+    val ds = CSVDataset("linear_function_1.scv")
+    ds.size should be(97)
+    val it = ds.iterator
+    it.next(2) should be(Tensor.matrix(
+      Array(6.1101f, 17.592f),
+      Array(5.5277f, 9.1302f)))
+  }
+
+  it should "fill last batch with zeros" in {
+    val it = CSVDataset("linear_function_1.scv").iterator
+    // rewind to last batch
+    for (_ <- 1 to 19) {
+      it.next(5)
+    }
+    it.next(5) should be(Tensor.matrix(
+      Array(13.394f, 9.0551f),
+      Array(5.4369f, 0.61705f),
+      Array(0f, 0f),
+      Array(0f, 0f),
+      Array(0f, 0f)
+    ))
   }
 }
