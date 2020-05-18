d355abb1db7dbf3ebae139a5960518db08f4815c
pavlo <pavlo.pohrebnyi@asg.com>
2020-05-18 10:46:17 +0300

    Support of different shapes

--- src/main/scala/org/scanet/core/core.scala
+++ src/main/scala/org/scanet/core/core.scala
@@ -9,7 +9,9 @@ package object core {
   def error(message: String): Nothing = throw new RuntimeException(message)
 
   def memoize[I1, O](f: I1 => O): I1 => O = new mutable.HashMap[I1, O]() {self =>
-    override def apply(key: I1): O = getOrElseUpdate(key, f(key))
+    override def apply(key: I1): O = {
+      getOrElseUpdate(key, f(key))
+    }
   }
 
   def memoize[I1, I2, O](f: (I1, I2) => O): (I1, I2) => O = {
--- src/test/scala/org/scanet/core/TFSpec.scala
+++ src/test/scala/org/scanet/core/TFSpec.scala
@@ -1,5 +1,7 @@
 package org.scanet.core
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scanet.core.Session.using
 import org.scanet.core.Tensor.scalar
@@ -8,10 +10,23 @@ import org.scanet.math.syntax._
 
 class TFSpec extends AnyFlatSpec with CustomMatchers {
 
+  "memoization" should "cache result of the function and reuse it" in {
+    val counter = new AtomicInteger(0)
+    val plus = memoize((a: Int, b: Int) => {
+      counter.incrementAndGet()
+      a + b
+    })
+    counter.get() should be(0)
+    plus(2, 3) should be(5)
+    counter.get() should be(1)
+    plus(2, 3) should be(5)
+    counter.get() should be(1)
+  }
+
   "tensor function composition" should "work" in {
     val sqr: TF1[Int, Int] = TF1(shape => {
       val arg = placeholder[Int](shape)
-      val result = arg * arg
+      val result = arg + arg
       (arg, result)
     })
     val identity: TF1[Int, Int] = TF1(shape => {
@@ -24,8 +39,9 @@ class TFSpec extends AnyFlatSpec with CustomMatchers {
       val func = leftPlusRightSqr.compile(session)
       // we can call the function multiple times now
       // that is compiled and same session will be reused
-      func(scalar(5), scalar(3)) should be(scalar(14))
-      func(scalar(2), scalar(6)) should be(scalar(38))
+      func(scalar(5), scalar(3)) should be(scalar(11))
+      // cool thing here is if we call the function with args which have
+      // different shape - the new graph will be compiled and also cached
     })
   }
 
