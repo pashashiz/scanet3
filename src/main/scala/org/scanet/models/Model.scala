9ee68885027d36aff6ea0b6d0ea74eb698ca3c51
Pavlo Pohrebnyi <pogrebnij@gmail.com>
2020-05-22 12:21:08 +0300

    Display model (#38)
    Display model

--- src/main/scala/org/scanet/models/Model.scala
+++ src/main/scala/org/scanet/models/Model.scala
@@ -12,4 +12,9 @@ case class Model[X: Numeric: TensorType, W: Numeric: TensorType, J: Numeric: Ten
   def grad: TF2[X, W, (Output[W], Output[Float]), (Tensor[W], Tensor[Float])] =
     TF2((x: Output[X], w: Output[W]) => (w, builder(x, w).grad(w))).returns[(Tensor[W], Tensor[Float])]
 
+  def display(dir: String = "", x: Shape = Shape(1, 3), w: Shape = Shape(3)): Unit = {
+    val (_, _, resultOut) = result.builder(x, w)
+    val (_, _, (_, gradOut)) = grad.builder(x, w)
+    (resultOut.as("result"), gradOut.as("grad")).display(dir)
+  }
 }
