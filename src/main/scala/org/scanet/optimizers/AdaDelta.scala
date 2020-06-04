bafff5ca5c10d0a18d6ffbd2b84d9f97cff7a939
Yurii <yurii.atamanchuk@zebra.com>
2020-06-05 01:12:24 +0300

    minor nothing

--- src/main/scala/org/scanet/optimizers/AdaDelta.scala
+++ src/main/scala/org/scanet/optimizers/AdaDelta.scala
@@ -3,7 +3,7 @@ package org.scanet.optimizers
 import org.scanet.core._
 import org.scanet.syntax._
 
-case class AdaDelta(rate: Float = 1, rho: Float = 0.9F, epsilon: Float = 1e-7F) extends Algorithm {
+case class AdaDelta(rate: Float = 1, rho: Float = 0.9f) extends Algorithm {
 
   def initMeta[X: TensorType](initArg: Tensor[X]): Tensor[Float] = {
     Tensor.zeros[Float](Shape(2 :: initArg.shape.dims))
@@ -12,7 +12,7 @@ case class AdaDelta(rate: Float = 1, rho: Float = 0.9F, epsilon: Float = 1e-7F) 
   def delta(grad: Output[Float], meta: Output[Float]): Delta = {
     // root mean squared
     def rms(x: Output[Float]): Output[Float] =
-      (x + epsilon.const).sqrt
+      (x + 1e-7f.const).sqrt
     // running (decaying) average
     def avg(prev: Output[Float], curr: Output[Float]): Output[Float] =
       rho.const * prev + (1 - rho).const * curr.pow(2)
