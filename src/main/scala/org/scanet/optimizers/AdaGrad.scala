dd65c795e70fa4427a38b2522d673184446133d6
pavlo <pavlo.pohrebnyi@asg.com>
2020-06-05 22:44:01 +0300

    Added benchmark

--- src/main/scala/org/scanet/optimizers/AdaGrad.scala
+++ src/main/scala/org/scanet/optimizers/AdaGrad.scala
@@ -3,7 +3,15 @@ package org.scanet.optimizers
 import org.scanet.core._
 import org.scanet.syntax._
 
-case class AdaGrad(rate: Double = 1) extends Algorithm {
+/**
+ * Adagrad is an optimizer with parameter-specific learning rates,
+ * which are adapted relative to how frequently a parameter gets updated during training.
+ * The more updates a parameter receives, the smaller the updates.
+ *
+ * @param rate the learning rate.
+ * @param epsilon a constant epsilon used to better conditioning the grad update
+ */
+case class AdaGrad(rate: Double = 1.0, epsilon: Double = 1e-7) extends Algorithm {
 
   def initMeta(shape: Shape): Tensor[Float] = Tensor.zeros[Float](shape)
 
@@ -11,7 +19,7 @@ case class AdaGrad(rate: Double = 1) extends Algorithm {
     // we accumulate all squared gradient per each weight
     val gradAcc = prevGradAcc + grad.sqr
     // the larger gradient is accumulated the lower rate is applied for a given weight
-    val rates = rate.toFloat.const / (gradAcc.sqrt + 1e-7f.const)
+    val rates = rate.toFloat.const / (gradAcc.sqrt + epsilon.toFloat.const)
     val delta = rates :* grad
     Delta(delta, gradAcc)
   }
--- src/test/scala/org/scanet/optimizers/SGDSpec.scala
+++ src/test/scala/org/scanet/optimizers/SGDSpec.scala
@@ -1,11 +1,71 @@
 package org.scanet.optimizers
 
 import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.should.Matchers
+import org.scanet.core.Tensor
+import org.scanet.datasets.{CSVDataset, EmptyDataset}
+import org.scanet.math.syntax._
+import org.scanet.models.Regression
+import org.scanet.optimizers.syntax._
+import org.scanet.test.CustomMatchers
+import org.scanet.models.Math._
 
-class SGDSpec extends AnyFlatSpec with Matchers {
+class SGDSpec extends AnyFlatSpec with CustomMatchers {
 
-  "SDG" should "calculate optimization step" in {
-    // todo
+  "Plain SGD" should "minimize x^2" in {
+    val opt = Optimizer
+      .minimize(`x^2`)
+      .using(SGD(rate = 0.1))
+      .initWith(Tensor.scalar(5.0f))
+      .on(EmptyDataset())
+      .epochs(20)
+      .build
+    val x = opt.run()
+    val y = `x^2`.result.compile()(Tensor.scalar(0.0f), x)
+    y.toScalar should be <= 0.1f
+  }
+
+  "Plain SGD" should "minimize linear regression" in {
+    val ds = CSVDataset("linear_function_1.scv")
+    val weights = Optimizer
+      .minimize(Regression.linear)
+      .using(SGD())
+      .initWith(s => Tensor.zeros(s))
+      .on(ds)
+      .stopAfter(1500.epochs)
+      .build
+      .run()
+    val regression = Regression.linear.result.compile()
+    val result = regression(ds.iterator.next(100), weights)
+    result.toScalar should be <= 4.5f
+  }
+
+  "SGD with momentum" should "minimize linear regression" in {
+    val ds = CSVDataset("linear_function_1.scv")
+    val weights = Optimizer
+      .minimize(Regression.linear)
+      .using(SGD(momentum = 0.1))
+      .initWith(s => Tensor.zeros(s))
+      .on(ds)
+      .stopAfter(1300.epochs)
+      .build
+      .run()
+    val regression = Regression.linear.result.compile()
+    val result = regression(ds.iterator.next(100), weights)
+    result.toScalar should be <= 4.5f
+  }
+
+  "SGD with Nesterov acceleration" should "minimize linear regression" in {
+    val ds = CSVDataset("linear_function_1.scv")
+    val weights = Optimizer
+      .minimize(Regression.linear)
+      .using(SGD(momentum = 0.1, nesterov = true))
+      .initWith(Tensor.zeros(_))
+      .on(ds)
+      .stopAfter(1300.epochs)
+      .build
+      .run()
+    val regression = Regression.linear.result.compile()
+    val result = regression(ds.iterator.next(100), weights)
+    result.toScalar should be <= 4.5f
   }
 }
