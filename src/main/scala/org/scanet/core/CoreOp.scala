55932f7f7eafc4b8d7a8dcb04a935fb6c2c8819c
pavlo <pavlo.pohrebnyi@asg.com>
2020-06-05 13:12:09 +0300

    Fix

--- src/main/scala/org/scanet/core/CoreOp.scala
+++ src/main/scala/org/scanet/core/CoreOp.scala
@@ -297,40 +297,6 @@ object CoreOp {
         })
         .build
     }
-
-    def reshape[A: TensorType](op: Output[A], shape: Shape): Output[A] = {
-      require(op.shape.power == shape.power ,
-        s"shape ${op.shape} cannot be reshaped into $shape")
-      if (op.shape != shape) {
-        // note: scalar is a special case, reshape does not work with scalars
-        if (shape.isScalar) {
-          squeeze(op)
-        } else {
-          Output.name[A]("Reshape")
-            .shape(shape)
-            .inputs(op, as(Tensor.vector(shape.dims: _*).const, "new_shape"))
-            .localGrad(ctx => List(reshape(ctx.parentGrad, op.shape)))
-            .compileWithAllInputs
-            .build
-        }
-      } else {
-        op
-      }
-    }
-
-    def squeeze[A: TensorType](op: Output[A]): Output[A] = {
-      val squeezed = op.shape.squeeze
-      if (squeezed.rank < op.shape.rank) {
-        Output.name[A]("Squeeze")
-          .shape(squeezed)
-          .inputs(op)
-          .localGrad(ctx => List(reshape(ctx.parentGrad, op.shape)))
-          .compileWithAllInputs
-          .build
-      } else {
-        op
-      }
-    }
   }
 
   trait ThenStep {
