e6176d81ae0c8536caa1bdca710d3d621a1baec1
Pavlo Pohrebnyi <pogrebnij@gmail.com>
2020-06-05 22:52:46 +0300

    Added benchmark (#49)
    Co-authored-by: pavlo <pavlo.pohrebnyi@asg.com>

--- src/main/scala/org/scanet/optimizers/AdaDelta.scala
+++ src/main/scala/org/scanet/optimizers/AdaDelta.scala
@@ -3,24 +3,42 @@ package org.scanet.optimizers
 import org.scanet.core._
 import org.scanet.syntax._
 
-case class AdaDelta(rate: Float = 1, rho: Float = 0.9f) extends Algorithm {
+/**
+ * `Adadelta` optimization is a stochastic gradient descent method that is based on adaptive
+ * learning rate per dimension to address two drawbacks:
+ * - The continual decay of learning rates throughout training
+ * - The need for a manually selected global learning rate
+ *
+ * `Adadelta` is a more robust extension of `Adagrad` that adapts learning rates based
+ * on a moving window of gradient updates, instead of accumulating all past gradients. T
+ * his way, `Adadelta` continues learning even when many updates have been done.
+ * Compared to `Adagrad`, in the original version of `Adadelta` you don't have to set an initial learning rate.
+ *
+ * In this version, initial learning rate can be set, as in most other optimizers.
+ *
+ * @param rate learning rate, usually should not be tuned
+ * @param rho the decay rate
+ * @param epsilon a constant epsilon used to better conditioning the grad update
+ */
+case class AdaDelta(rate: Double = 1.0, rho: Double = 0.9, epsilon: Double = 1e-7) extends Algorithm {
 
   def initMeta(shape: Shape): Tensor[Float] = {
-    val arg = Tensor.zeros[Float](shape).const
-    arg.zip(arg).eval
+    val arg1 = Tensor.zeros[Float](shape).const
+    val arg2 = Tensor.zeros[Float](shape).const
+    arg1.zip(arg2).eval
   }
 
   def delta(grad: Output[Float], meta: Output[Float]): Delta = {
     // root mean squared
     def rms(x: Output[Float]): Output[Float] =
-      (x + 1e-7f.const).sqrt
+      (x + epsilon.toFloat.const).sqrt
     // running (decaying) average
     def avg(prev: Output[Float], curr: Output[Float]): Output[Float] =
-      rho.const * prev + (1 - rho).const * curr.sqr
+      rho.toFloat.const * prev + (1 - rho.toFloat).const * curr.sqr
 
     val (prevAvgGrad, prevAvgDelta) = meta.unzip
     val avgGrad = avg(prevAvgGrad, grad)
-    val delta = rate.const * ((rms(prevAvgDelta) / rms(avgGrad)) :* grad)
+    val delta = rate.toFloat.const * ((rms(prevAvgDelta) / rms(avgGrad)) :* grad)
     val avgDelta = avg(prevAvgDelta, delta)
     Delta(delta, avgGrad.zip(avgDelta))
   }
---
+++ src/test/scala/org/scanet/benchmarks/RegressionBenchmark.scala
@@ -0,0 +1,100 @@
+package org.scanet.benchmarks
+
+import org.scalatest.Ignore
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scanet.datasets.CSVDataset
+import org.scanet.math.syntax._
+import org.scanet.models.Regression
+import org.scanet.optimizers.Effect.{logResult, plotResult}
+import org.scanet.optimizers.syntax._
+import org.scanet.optimizers.{AdaDelta, AdaGrad, Optimizer, SGD}
+import org.scanet.test.CustomMatchers
+
+@Ignore
+class RegressionBenchmark extends AnyFlatSpec with CustomMatchers {
+
+  "linear regression" should "be minimized by plain SDG" in {
+    val ds = CSVDataset("facebook-comments-scaled.csv")
+    val weights = Optimizer
+      .minimize(Regression.linear)
+      .using(SGD())
+      .on(ds)
+      .batch(1000)
+      .each(1.epochs, logResult())
+      .each(1.iterations, plotResult(name = "Error", dir = "board/SDG"))
+      .stopAfter(5.epochs)
+      .build
+      .run()
+    val regression = Regression.linear.result.compile()
+    val result = regression(ds.iterator.next(1000), weights)
+    result.toScalar should be <= 1f
+  }
+
+  it should "be minimized by SDG with momentum" in {
+    val ds = CSVDataset("facebook-comments-scaled.csv")
+    val weights = Optimizer
+      .minimize(Regression.linear)
+      .using(SGD(momentum = 0.9))
+      .on(ds)
+      .batch(1000)
+      .each(1.epochs, logResult())
+      .each(1.iterations, plotResult(name = "Error", dir = "board/Momentum"))
+      .stopAfter(5.epochs)
+      .build
+      .run()
+    val regression = Regression.linear.result.compile()
+    val result = regression(ds.iterator.next(1000), weights)
+    result.toScalar should be <= 1f
+  }
+
+  it should "be minimized by SDG with nesterov acceleration" in {
+    val ds = CSVDataset("facebook-comments-scaled.csv")
+    val weights = Optimizer
+      .minimize(Regression.linear)
+      .using(SGD(momentum = 0.9, nesterov = true))
+      .on(ds)
+      .batch(1000)
+      .each(1.epochs, logResult())
+      .each(1.iterations, plotResult(name = "Error", dir = "board/Nesterov"))
+      .stopAfter(5.epochs)
+      .build
+      .run()
+    val regression = Regression.linear.result.compile()
+    val result = regression(ds.iterator.next(1000), weights)
+    result.toScalar should be <= 1f
+  }
+
+  it should "be minimized by AdagGrad" in {
+    val ds = CSVDataset("facebook-comments-scaled.csv")
+    val weights = Optimizer
+      .minimize(Regression.linear)
+      .using(AdaGrad())
+      .on(ds)
+      .batch(1000)
+      .each(1.epochs, logResult())
+      .each(1.iterations, plotResult(name = "Error", dir = "board/AdagGrad"))
+      .stopAfter(5.epochs)
+      .build
+      .run()
+    val regression = Regression.linear.result.compile()
+    val result = regression(ds.iterator.next(1000), weights)
+    result.toScalar should be <= 0.5f
+  }
+
+  it should "be minimized by AdagDelta" in {
+    val ds = CSVDataset("facebook-comments-scaled.csv")
+    val weights = Optimizer
+      .minimize(Regression.linear)
+      .using(AdaDelta())
+      .on(ds)
+      .batch(1000)
+      .each(1.epochs, logResult())
+      .each(1.iterations, plotResult(name = "Error", dir = "board/AdagDelta"))
+      .stopAfter(20.epochs)
+      .build
+      .run()
+    val regression = Regression.linear.result.compile()
+    val result = regression(ds.iterator.next(1000), weights)
+    result.toScalar should be <= 0.5f
+  }
+}
