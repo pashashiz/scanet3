58f3a2f3ef419887a599f2975e3ec0603c57e89e
pashashiz <pogrebnij@gmail.com>
2020-07-06 21:52:02 +0300

    Few tests

--- src/test/scala/org/scanet/core/TFxSpec.scala
+++ src/test/scala/org/scanet/core/TFxSpec.scala
@@ -7,12 +7,10 @@ import org.scanet.core.Session.syntaxX._
 import org.scanet.core.Session.withing
 import org.scanet.core.Tensor.scalar
 
+/*_*/
 class TFxSpec extends AnyFlatSpec with CustomMatchers {
 
-  // todo: test on Seq input and output
-
   "tensor function of 1 arg" should "support complex result type" in {
-    /*_*/
     val duplicate = TFx1((arg: Id[Output[Int]]) =>
       ((0.const + arg).toId, (0.const + arg).toId))
       .returns[(Id[Tensor[Int]], Id[Tensor[Int]])]
@@ -24,33 +22,50 @@ class TFxSpec extends AnyFlatSpec with CustomMatchers {
       // different shape - the new graph will be compiled and also cached
       func(scalar(5)) should be((scalar(5), scalar(5)))
     })
-    /*_*/
+  }
+
+  it should "support sequence arg type" in {
+    val f = TFx1((arg: Seq[Output[Int]]) => plus(arg: _*).toId).returns[Id[Tensor[Int]]]
+    val total = f.compile()
+    total(Seq(scalar(1), scalar(3), scalar(5))) should be(scalar(9))
+  }
+
+  it should "support sequence result type" in {
+    val f = TFx1((arg: Id[Output[Int]]) =>
+      Seq(fromId(arg) + 0.const, fromId(arg) + 0.const))
+      .returns[Seq[Tensor[Int]]]
+    val double = f.compile()
+    double(scalar(1)) should be(Seq(scalar(1), scalar(1)))
+  }
+
+  it should "support sequence result type inside a tuple" in {
+    val f = TFx1((arg: Id[Output[Int]]) => {
+      val a: Output[Int] = arg
+      (1f.const.toId, Seq(a + 0.const, a + 0.const))
+    }).returns[(Id[Tensor[Float]], Seq[Tensor[Int]])]
+    val double = f.compile()
+    double(scalar(1)) should be((scalar(1f), Seq(scalar(1), scalar(1))))
   }
 
   "tensor function of 2 args" should "work" in {
-    /*_*/
     val plus = TFx2((arg1: Id[Output[Int]], arg2: Id[Output[Int]]) =>
       (fromId(arg1) + arg2).toId).returns[Id[Tensor[Int]]]
     withing(session => {
       val func = plus compile session
       func(scalar(2), scalar(3)) should be(scalar(5))
     })
-    /*_*/
   }
 
   "tensor function of 3 args" should "work" in {
-    /*_*/
     val plus = TFx3((arg1: Id[Output[Int]], arg2: Id[Output[Int]], arg3: Id[Output[Int]]) =>
       (fromId(arg1) + arg2 + arg3).toId).returns[Id[Tensor[Int]]]
     withing(session => {
       val func = plus compile session
       func(scalar(2), scalar(3), scalar(4)) should be(scalar(9))
     })
-    /*_*/
   }
 
   it should "support composition with other 2 arg function" in {
-    /*_*/
     val plus = TFx3((arg1: Id[Output[Int]], arg2: Id[Output[Int]], arg3: Id[Output[Int]]) =>
       (fromId(arg1) + arg2 + arg3).toId)
       .returns[Id[Tensor[Int]]]
@@ -63,11 +78,9 @@ class TFxSpec extends AnyFlatSpec with CustomMatchers {
       val func = leftMultiplyRightSum compile session
       func(scalar(1), scalar(2), scalar(3), scalar(4), scalar(2)) should be(scalar(12))
     })
-    /*_*/
   }
 
   "tensor function of 4 args" should "work" in {
-    /*_*/
     val plus = TFx4((arg1: Id[Output[Int]], arg2: Id[Output[Int]], arg3: Id[Output[Int]], arg4: Id[Output[Int]]) =>
       (fromId(arg1) + arg2 + arg3 + arg4).toId)
       .returns[Id[Tensor[Int]]]
@@ -75,11 +88,9 @@ class TFxSpec extends AnyFlatSpec with CustomMatchers {
       val func = plus compile session
       func(scalar(2), scalar(3), scalar(4), scalar(5)) should be(scalar(14))
     })
-    /*_*/
   }
 
   "tensor function of 5 args" should "work" in {
-    /*_*/
     val plus = TFx5((arg1: Id[Output[Int]], arg2: Id[Output[Int]], arg3: Id[Output[Int]], arg4: Id[Output[Int]], arg5: Id[Output[Int]]) =>
       (fromId(arg1) + arg2 + arg3 + arg4 + arg5).toId)
       .returns[Id[Tensor[Int]]]
@@ -87,6 +98,6 @@ class TFxSpec extends AnyFlatSpec with CustomMatchers {
       val func = plus compile session
       func(scalar(2), scalar(3), scalar(4), scalar(5), scalar(6)) should be(scalar(20))
     })
-    /*_*/
   }
 }
+/*_*/
