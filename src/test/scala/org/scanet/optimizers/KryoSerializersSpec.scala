f55276d747cd2736bb545397761d9033be4711b1
Pavlo Pohrebnyi <ppoh@softserveinc.com>
2021-08-21 21:27:36 +0300

    Fixed kryo serializarion

--- src/test/scala/org/scanet/optimizers/KryoSerializersSpec.scala
+++ src/test/scala/org/scanet/optimizers/KryoSerializersSpec.scala
@@ -1,7 +1,22 @@
 package org.scanet.optimizers
 
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.wordspec.AnyWordSpec
+import org.scanet.core.Tensor
+import org.scanet.core.syntax._
+import org.scanet.test.KryoMatchers
 
-class KryoSerializersSpec extends AnyFlatSpec {
-  // todo: test string serialization
+class KryoSerializersSpec extends AnyWordSpec with KryoMatchers {
+
+  val kryo = KryoSerializers.Kryo
+
+  "kryo" should {
+
+    "write/read the tensor of floats" in {
+      Tensor.vector(1f, 2f, 3f) should beSerializableAs[Tensor[Float]](using = kryo)
+    }
+
+    "write/read the tensor of strings" in {
+      Tensor.vector("hey", "dawg") should beSerializableAs[Tensor[String]](using = kryo)
+    }
+  }
 }
