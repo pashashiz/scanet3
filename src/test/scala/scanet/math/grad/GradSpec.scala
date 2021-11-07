21d5e9f4025fee0aea2ee1f03d182ed87c63b47d
Pavlo Pohrebnyi <ppoh@softserveinc.com>
2021-11-07 18:07:50 +0200

    Jacobian idea

--- src/test/scala/scanet/math/grad/GradSpec.scala
+++ src/test/scala/scanet/math/grad/GradSpec.scala
@@ -2,7 +2,7 @@ package scanet.math.grad
 
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-import scanet.core.OutputSeq
+import scanet.core.{Expr, OutputSeq, Tensor}
 import scanet.core.Session.withing
 import scanet.core.Tensor.scalar
 import scanet.math.syntax._
@@ -13,7 +13,9 @@ import scala.collection.immutable.Seq
 class GradSpec extends AnyWordSpec with Matchers {
 
   "gradient" when {
+
     "taken in respect to multiple inputs" should {
+
       "work" in {
         val a = 3f.const.as("a")
         val b = 4f.const.as("b")
@@ -21,6 +23,7 @@ class GradSpec extends AnyWordSpec with Matchers {
         val grads: OutputSeq[Float] = f.grad(Seq(a, b)).returns[Float]
         grads.eval should be(Seq(scalar(1728f), scalar(1296f)))
       }
+
       "reuse common operations of a computation graph" in {
         val a = 3f.const.as("a")
         val b = 4f.const.as("b")
@@ -35,6 +38,51 @@ class GradSpec extends AnyWordSpec with Matchers {
           val ops = graph.operations().asScala
           ops should have size 11
         })
+      }
+    }
+  }
+
+  "gradient" should {
+
+    "compute second partial derivative" in {
+      val x = 5f.const.as("x")
+      val y = x.sqr.as("y")
+      val dy_dx = y.grad(x).returns[Float]
+      val dy_dx2 = dy_dx.grad(x).returns[Float]
+      dy_dx2.eval should be(Tensor.scalar(2f))
+    }
+
+    "compute successive (mixed) partial derivative" in {
+      val x = 5f.const.as("x")
+      val y = 2f.const.as("y")
+      val z = (x.sqr * y.pow(3) + 3f.const * y + x + 5f.const).as("z")
+      val dz_dx = z.grad(x).returns[Float] // 2x * y^3 + 1
+      val dz_dx_dy = dz_dx.grad(y).returns[Float] // 6x * y^2
+      dz_dx_dy.eval should be(Tensor.scalar(120f))
+    }
+
+    "compute Jacobian matrix" in {
+
+      // given f(x, y)
+      // we can calc gradients [df/dx, df/dy]
+      // and if we calc Jacobian for those
+      // | d2f/dx_dx d2f/dx_dy |
+      // | d2f/dy_dx d2f/dy_dy |
+      // we will end up with Hessian
+
+      // NOTE: Postponed case requires lots of work and will not bring much value as of now
+      def jacobian[A](outputs: Expr[A], inputs: Expr[A]): Expr[A] = {
+        // To implement that in vectorized form we need to
+        // implement: flatten, gather and for loop kernels.
+        // So we could take a tensor of outputs
+        // flatten it, and for each i-th element gather it and find a gradient
+        // in regards to the inputs.
+        // At the end we need to join it back to a single tensor.
+        // See https://github.com/tensorflow/tensorflow/blob/master/tensorflow/python/ops/parallel_for/gradients.py#L24
+        // For neural network we can then easily find a hessian matrix
+        // val grads = loss.gradient(w)
+        // val hessian = jacobian(grads, w)
+        ???
       }
     }
   }
